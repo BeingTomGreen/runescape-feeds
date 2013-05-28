3d3466d10448b6ccf2c0afa657ab6d7f25ef5239
Tom Green <kingofcuddles@gmail.com>
2013-05-28 19:05:57 +0100

    Fleshed out the about tab.

--- public/index.php
+++ public/index.php
@@ -45,7 +45,13 @@ $simplePie = new SimplePie();
 			  <div class="tab-pane active" id="rsnews">xxx</div>
 			  <div class="tab-pane" id="rsblogs">yyy</div>
 			  <div class="tab-pane" id="rssocial">uuu</div>
-			  <div class="tab-pane" id="about">ccc</div>
+			  <div class="tab-pane" id="about">
+					<p>This page is designed to provided people with the latest goings on in <a href="http://www.runescape.com" title="Runescape homepage">Runescape</a><p>
+					<p>This page was built using <a href="http://simplepie.org/" title="The SimplePie website">SimplePie</a> and the <a href="http://twitter.github.io/bootstrap/" title="Bootstrap!">Twitter Bootstrap</a>.</p>
+					<p>You can find the source code for this on <a href='https://bitbucket.org/BeingTomGreen/runescape_feeds'>Bitbucket</a>.</p>
+					<p><a href='https://bitbucket.org/BeingTomGreen/runescape_feeds/issues'>Found a bug?</a></p>
+					<p>Built by <a href='https://twitter.com/beingtomgreen'>@beingtomgreen</a></p>
+				</div>
 			</div>
 		</div>
 
