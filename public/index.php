97d836861de59dd90fab29eaef884d9c114dbf3e
Tom Green <kingofcuddles@gmail.com>
2013-05-28 19:46:44 +0100

    Added cache folder. Had first tab display on load.

--- public/index.php
+++ public/index.php
@@ -34,20 +34,20 @@ $simplePie = new SimplePie();
 
 	<body>
 		<div id='wrapper'>
-			<ul class="nav nav-tabs">
-				<li><a href="#rsnews" data-toggle="tab">Runescape News</a></li>
-				<li><a href="#rsblogs" data-toggle="tab">Runescape Blogs</a></li>
-				<li><a href="#rssocial" data-toggle="tab">Runescape Social</a></li>
-			 	<li><a href="#about" data-toggle="tab">About</a></li>
+			<ul class='nav nav-tabs'  id='tabs'>
+				<li><a href='#rsnews' data-toggle='tab'>Runescape News</a></li>
+				<li><a href='#rsblogs' data-toggle='tab'>Runescape Blogs</a></li>
+				<li><a href='#rssocial' data-toggle='tab'>Runescape Social</a></li>
+			 	<li><a href='#about' data-toggle='tab'>About</a></li>
 			</ul>
 
-			<div class="tab-content">
-			  <div class="tab-pane active" id="rsnews">xxx</div>
-			  <div class="tab-pane" id="rsblogs">yyy</div>
-			  <div class="tab-pane" id="rssocial">uuu</div>
-			  <div class="tab-pane" id="about">
-					<p>This page is designed to provided people with the latest goings on in <a href="http://www.runescape.com" title="Runescape homepage">Runescape</a><p>
-					<p>This page was built using <a href="http://simplepie.org/" title="The SimplePie website">SimplePie</a> and the <a href="http://twitter.github.io/bootstrap/" title="Bootstrap!">Twitter Bootstrap</a>.</p>
+			<div class='tab-content'>
+			  <div class='tab-pane active' id='rsnews'>xxx</div>
+			  <div class='tab-pane' id='rsblogs'>yyy</div>
+			  <div class='tab-pane' id='rssocial'>uuu</div>
+			  <div class='tab-pane' id='about'>
+					<p>This page is designed to provided people with the latest goings on in <a href='http://www.runescape.com' title='Runescape homepage'>Runescape</a><p>
+					<p>This page was built using <a href='http://simplepie.org/' title='The SimplePie website'>SimplePie</a> and the <a href='http://twitter.github.io/bootstrap/' title='Bootstrap!'>Twitter Bootstrap</a>.</p>
 					<p>You can find the source code for this on <a href='https://bitbucket.org/BeingTomGreen/runescape_feeds'>Bitbucket</a>.</p>
 					<p><a href='https://bitbucket.org/BeingTomGreen/runescape_feeds/issues'>Found a bug?</a></p>
 					<p>Built by <a href='https://twitter.com/beingtomgreen'>@beingtomgreen</a></p>
@@ -59,5 +59,10 @@ $simplePie = new SimplePie();
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
 		<script src="assets/js/bootstrap.min.js"></script>
+		<script>
+  		$(function () {
+    		$('#tabs a:first').tab('show');
+  		})
+		</script>
 	</body>
 </html>
